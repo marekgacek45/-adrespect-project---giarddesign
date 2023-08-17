d7153eef7c225a2bcb8853625612083951bd91be
marekgacek45 <marekgacek45@gmail.com>
2023-08-17 13:07:45 +0200

    create slider on desktop

--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,69 @@ const dropdownHandler = () => {
 }
 
 
+//slider
+
+
+const slides = document.querySelectorAll('.sliderSlide');
+let currentSlide = 0;
+let interval;
+
+function showSlide(index) {
+    slides.forEach((slide, idx) => {
+        if (idx === index) {
+            slide.style.display = 'flex';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+function startAutoSlide() {
+    interval = setInterval(() => {
+        nextSlide();
+    }, 5000);
+}
+
+function stopAutoSlide() {
+    clearInterval(interval);
+}
+
+function startSlider() {
+    showSlide(currentSlide);
+
+    const nextButtons = document.querySelectorAll('.nextButton');
+    nextButtons.forEach(nextButton => {
+        nextButton.addEventListener('click', () => {
+            nextSlide();
+            stopAutoSlide();
+            startAutoSlide();
+        });
+    });
+
+    const prevButtons = document.querySelectorAll('.prevButton');
+    prevButtons.forEach(prevButton => {
+        prevButton.addEventListener('click', () => {
+            prevSlide();
+            stopAutoSlide();
+            startAutoSlide();
+        });
+    });
+
+    startAutoSlide();
+}
+
+startSlider();
+
 //listeners
 hamburgerBtn.addEventListener('click',navbarDropdownHandler)
 dropdownBtn.addEventListener('click', dropdownHandler);
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,10 @@ module.exports = {
       fontSize: {
         '28': '28px',
       },
+      backgroundImage: {
+        'slide-1': "url('/dist/img/slide1.jpg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
     },
   },
   plugins: [],
