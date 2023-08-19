e5ff45fdb8803c4fb5885f495f50b9bce72235f5
marekgacek45 <marekgacek45@gmail.com>
2023-08-19 08:33:50 +0200

    add contact section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html",  "./node_modules/flowbite/**/*.js"],
+  
   theme: {
     extend: {
       colors: {
@@ -40,6 +41,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
